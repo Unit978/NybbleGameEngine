5f7acd743edf1701c2f49e083e32f74212030b22
Unit978 <luislpz978@rocketmail.com>
2015-02-06 13:14:58 -0600

    Added variables to entity class for quick access to important components

--- NybbleGameEngine_0.1p3/nybble_engine/entity.py
+++ NybbleGameEngine_0.1p3/nybble_engine/entity.py
@@ -1,5 +1,9 @@
 import components
 from util_math import Vector2
+from components import Transform
+from components import RigidBody
+from components import Collider
+from components import Renderer
 
 
 class Entity (object):
@@ -9,13 +13,47 @@ class Entity (object):
         self.uuid = uuid
         self.components = list()
         self.tag = ""
+        self.name = ""
+        self.scripts = list()
+
+        # quick access to important components
+        self.transform = None
+        self.rigid_body = None
+        self.renderer = None
+        self.collider = None
 
     def add_component(self, component):
-        component.entity = self
+
+        # special components. Variable for quick access
+        if isinstance(component, Transform):
+            self.transform = component
+
+        elif isinstance(component, RigidBody):
+            self.rigid_body = component
+
+        elif isinstance(component, Collider):
+            self.collider = component
+
+        elif isinstance(component, Renderer):
+            self.renderer = component
+
         self.components.append(component)
 
     def remove_component(self, component):
         self.components.remove(component)
+
+    def add_script(self, script):
+        self.scripts.append(script)
+
+    def remove_script(self, script_name):
+        i = 0
+        for s in self.scripts:
+
+            # script found
+            if s.script_name == script_name:
+                self.scripts.pop(i)
+                return
+            i += 1
 
     def get_component(self, component_tag):
         for c in self.components:
@@ -37,9 +75,9 @@ class GameObject (Entity):
         # Set up pivot for the image
         pivot = Vector2(image_surface.get_width()/2, image_surface.get_height()/2)
 
-        self.add_component(components.TransformComponent())
-        self.add_component(components.RenderComponent(image_surface, pivot))
-        self.add_component(components.CollisionBoxComponent())
+        self.add_component(components.Transform())
+        self.add_component(components.Renderer(image_surface, pivot))
+        self.add_component(components.BoxCollider())
 
 
 # A game object with only a transform and collision box components.
@@ -48,5 +86,5 @@ class BoxColliderGameObject (Entity):
     def __init__(self, width, height, uuid=0):
         super(BoxColliderGameObject, self).__init__(uuid)
 
-        self.add_component(components.TransformComponent())
-        self.add_component(components.CollisionBoxComponent(width, height))
+        self.add_component(components.Transform())
+        self.add_component(components.BoxCollider(width, height))
